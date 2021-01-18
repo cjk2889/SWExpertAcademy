cd54e496465027ac18a32b23ab5acd5da54c1975
cjk2889 <cjk2889@gmail.com>
2021-01-19 06:02:46 +0900

    fail

--- 1770/usercode.cpp
+++ 1770/usercode.cpp
@@ -10,36 +10,32 @@ typedef unsigned long long int u64;
 
 typedef struct Block{
     int block_num;
+
     u64 module;
-    u64 reverse;
     
     int max;
     int min;
-    bool used;
     
     Block &operator=(Block ex)
     {
         block_num = ex.block_num;
         module = ex.module;
-        reverse = ex.reverse;
 
         max = ex.max;
         min = ex.min;
-        used = ex.used;
         return *this;
     }
 } Block;
 
 void quickSort(int first, int last);
 int binarySearch(int low, int high, u64 target);
-u64 getRotate90(u64 input); // rotate 90 degree
 u64 arr2u64(int module[4][4]); // int array -> uint64
 void u642arr(u64 in, char out[16]); // uint64 -> int array
 
 extern int makeBlock(int module[][4][4]);
 
 Block b[MAX];
-Block reverse_b[MAX*4 + 4];
+Block reverse_b[MAX*4];
 
 int main(void)
 {
@@ -56,9 +52,7 @@ int main(void)
 			{
 				for (int x = 0; x < 4; x++)
 				{
-					module[c][y][x] = base + (rand() % 3); // 1 ~ 
-                    if(module[c][y][x] >= 9)
-                        cout << "module num ::" << c << ":" << y << ":" << x << endl;
+					module[c][y][x] = base + (rand() % 3); // 1 ~ 8
 				}
 			}
         }
@@ -82,11 +76,11 @@ void quickSort(int first, int last)
  
         while (i < j)
         {
-            while (reverse_b[i].reverse >= reverse_b[pivot].reverse && i < last)
+            while (reverse_b[i].module <= reverse_b[pivot].module && i < last)
             {
                 i++;
             }
-            while (reverse_b[j].reverse < reverse_b[pivot].reverse)
+            while (reverse_b[j].module > reverse_b[pivot].module)
             {
                 j--;
             }
@@ -109,37 +103,28 @@ void quickSort(int first, int last)
 
 int binarySearch(int low, int high, u64 target)
 {
-    int mid;
+    int mid = (low + high) / 2;
     if (low > high) 
     {
         return -1;
     }
- 
-    mid = (low + high) / 2;
- 
-    if (target < reverse_b[mid].reverse)
+    while (low <= high)
     {
-        return binarySearch(low, mid - 1, target);
+        mid = (low + high) / 2;
+        if (target == reverse_b[mid].module)
+        {
+            return reverse_b[mid].block_num;
+        }
+        else if (target < reverse_b[mid].module)
+        {
+            high = mid - 1;
+        }
+        else if (target > reverse_b[mid].module)
+        {
+            low = mid + 1;
+        }
     }
-    else if (reverse_b[mid].reverse < target)
-    {
-        return binarySearch(mid + 1, high, target);
-    }
-    else
-    {
-		int index = reverse_b[mid].block_num;
-
-		reverse_b[mid].used = true;
-		b[index].used = true;
-
-        return mid;
-    }
-}
- 
-
-u64 getRotate90(u64 input){
-    u64 ret = input % 1000000000000 * 10000 + (input/1000000000000);
-    return ret;
+    return -1;
 }
 
 u64 arr2u64(int module[4][4]){
@@ -165,63 +150,72 @@ void u642arr(u64 in, char out[16]){
 
 int makeBlock(int module[][4][4]){
     int ret = 0;
+    bool check_used[MAX] = {false,};
 
     for (int c = 0; c < MAX; c++)
     {
+        check_used[c] = false;
+
         b[c].block_num = c;
         b[c].module = 0;
-        b[c].reverse = 0;
 
         b[c].max = module[c][0][0];
         b[c].min = module[c][0][0];
-        b[c].used = false;
+
+        reverse_b[c * 4 + 0].module = 0;
+        reverse_b[c * 4 + 1].module = 0;
+        reverse_b[c * 4 + 2].module = 0;
+        reverse_b[c * 4 + 3].module = 0;
 
 		for (int y = 0; y < 4; y++)
         {
             for (int x = 0; x < 4; x++)
             {
                 b[c].module += module[c][y][x]; b[c].module *= 10;
-                b[c].reverse += module[c][y][3-x];  b[c].reverse *= 10;
-                b[c].min = (b[c].min >  module[c][y][x])?  module[c][y][x] : b[c].min;
+                b[c].min = (b[c].min > module[c][y][x])? module[c][y][x] : b[c].min;
                 b[c].max = (b[c].max < module[c][y][x])? module[c][y][x] : b[c].max;
+
+                reverse_b[c * 4 + 0].module += module[c][3 - x][3 - y]; reverse_b[c * 4 + 0].module *= 10;
+                reverse_b[c * 4 + 1].module += module[c][  y  ][3 - x]; reverse_b[c * 4 + 1].module *= 10;
+                reverse_b[c * 4 + 2].module += module[c][  x  ][  y  ]; reverse_b[c * 4 + 2].module *= 10;
+                reverse_b[c * 4 + 3].module += module[c][3 - y][  x  ]; reverse_b[c * 4 + 3].module *= 10;
+
+                reverse_b[c * 4 + 0].block_num = c;
+                reverse_b[c * 4 + 1].block_num = c;
+                reverse_b[c * 4 + 2].block_num = c;
+                reverse_b[c * 4 + 3].block_num = c;
             }
         }
         b[c].module /= 10;
-        b[c].reverse /= 10;
 
-		for(int i = 0; i < 4; i++)
-		{
-			reverse_b[c * 4 + i].block_num = c;
-			reverse_b[c * 4 + i].max = module[c][0][0];
-			reverse_b[c * 4 + i].min = module[c][0][0];
-			reverse_b[c * 4 + i].used = false;
-			reverse_b[c * 4 + i].module = b[c].module;
-			
-			reverse_b[c * 4 + i].reverse = (i == 0 ) ? b[c].reverse : getRotate90(reverse_b[c * 4 + i-1].reverse);
-		}
+        reverse_b[c * 4 + 0].module /= 10;
+        reverse_b[c * 4 + 1].module /= 10;
+        reverse_b[c * 4 + 2].module /= 10;
+        reverse_b[c * 4 + 3].module /= 10;
     }
 
-	if (binarySearch(0, MAX * 4 + 3, b[0].reverse) >= 0)
-	{
-		cout << b[0].reverse << endl;
-	}
-    quickSort(0,MAX*4 + 3);
-
+    quickSort(0,MAX * 4 - 1);
 
 	for (int c = 0; c < MAX; c++)
-    {
-        if(b[c].used == true)
+    {   
+        if(check_used[c] == true)
             c++;
-        
-		int block_height = 16;
-		u64 target_module = block_height * (1111111111111111) - b[c].module;
-		for(;block_height >=3 || block_height >= b[c].max; block_height--){
-			if(binarySearch(0,MAX * 4 + 3, b[c].reverse) >= 0){
-				ret += block_height;
-				break;
-			}
-			target_module -= 1111111111111111;
-		}
+
+        int block_height = 8;
+        u64 target_module = (b[c].max + 6) * (1111111111111111) - b[c].module;
+
+        for (; block_height >= 2 ; block_height--)
+        {
+            int searchResult = binarySearch(0, MAX * 4 - 1, target_module);
+            if (searchResult >= 0 && check_used[searchResult] != true)
+            {
+                ret += target_module % 10 + b[c].module % 10;
+                check_used[searchResult] = true;
+                check_used[c] = true;
+                break;
+            }
+            target_module -= 1111111111111111;
+        }
     }
 
 
